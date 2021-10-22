b3a0545d2b3138f4fc88f027466ee8a1a7b71e6e
harisov-mobile <v8spec@mail.ru>
2021-10-22 09:11:38 +0300

    Добавил экран отображения детальной информации о фильме.

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -1,6 +1,110 @@
 package ru.androidschool.intensiv.ui.movie_details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatRatingBar
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import kotlinx.android.synthetic.main.item_with_text.*
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Actor
+import ru.androidschool.intensiv.data.MockRepository
+import ru.androidschool.intensiv.ui.feed.ActorItem
+import ru.androidschool.intensiv.ui.feed.FeedFragment
+import java.util.*
 
-class MovieDetailsFragment : Fragment(R.layout.movie_details_fragment)
+class MovieDetailsFragment : Fragment() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var overviewTextView: TextView
+    private lateinit var studioTextView: TextView
+    private lateinit var genreTextView: TextView
+    private lateinit var releaseDateTextView: TextView
+    private lateinit var imagePreview: ImageView
+    private lateinit var movieRating: AppCompatRatingBar
+    private lateinit var actorListRecyclerView: RecyclerView
+
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // так как Гугл рекомендовал отказаться от "синтетиков", я сделал по-старому через findViewById
+
+        val view = inflater.inflate(R.layout.movie_details_fragment, container, false)
+
+        titleTextView = view.findViewById(R.id.title_text_view)
+        overviewTextView = view.findViewById(R.id.overview_text_view)
+        studioTextView = view.findViewById(R.id.studio_text_view)
+        genreTextView = view.findViewById(R.id.genre_text_view)
+        releaseDateTextView = view.findViewById(R.id.release_date_text_view)
+        imagePreview = view.findViewById(R.id.image_preview)
+        movieRating = view.findViewById(R.id.movie_rating)
+        actorListRecyclerView = view.findViewById(R.id.actor_list_recycler_view)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val movieTitle = requireArguments().getString(FeedFragment.KEY_TITLE)
+
+        val movie = MockRepository.getMovies().find { it.title == movieTitle }
+
+        movie?.let {
+            titleTextView.text = movie.title
+            overviewTextView.text = movie.overview
+            studioTextView.text = movie.studio
+            movieRating.rating = movie.rating
+
+            val cal = Calendar.getInstance()
+            cal.time = movie.releaseDate
+
+            releaseDateTextView.text = cal.get(Calendar.YEAR).toString()
+
+            val genreListSize = movie.genreList.size
+            var genreString = ""
+            for (i in 0..(genreListSize-1)) {
+                genreString = genreString + ", " + movie.genreList[i].name // хитрый ход - чтобы в конце не обрезать, сделаю запятую с пробелом вначале, а потом с начала и удалю
+            }
+            genreString = genreString.substringAfter(' ')
+
+            genreTextView.text = genreString
+
+            // получаем список актеров из фильма и "приготавливаем" для Groupie
+            val actorItemList = movie.actorList.map {
+                ActorItem(it) { actor -> openActorDetails(actor) } // если понадобится открыть фрагмент с описанием актера
+            }.toList()
+
+            actorListRecyclerView.adapter = adapter.apply { addAll(actorItemList) }
+
+            Picasso.get()
+                .load(movie.previewUrl)
+                .into(imagePreview)
+        }
+    }
+
+    private fun openActorDetails(actor: Actor) {
+//        val bundle = Bundle()
+//        bundle.putString(KEY_ACTOR_ID, actor.id)
+//        findNavController().navigate(R.id.actor_details_fragment, bundle, options)
+        Toast.makeText(context, "" + actor.name + " " + actor.surname, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+
+        const val KEY_ACTOR_ID = "actor_id"
+    }
+}
+
+
--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -1,45 +1,51 @@
 package ru.androidschool.intensiv.ui.tvshows
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import kotlinx.android.synthetic.main.tv_shows_fragment.*
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.MockRepository
+import ru.androidschool.intensiv.data.TvShow
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TvShowsFragment : Fragment(R.layout.tv_shows_fragment) {
 
-class TvShowsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val options = navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tv_shows_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Используя Мок-репозиторий получаем фэйковый список тв-шоу
+        val tvShowList = MockRepository.getTvShows().map {
+                    TvShowItem(it) { tvShow -> openTvShowDetails(tvShow) }
+                }.toList()
+
+        tvshows_recycler_view.adapter = adapter.apply { addAll(tvShowList) }
+    }
+
+    private fun openTvShowDetails(tvShow: TvShow) {
+        val bundle = Bundle()
+        bundle.putString(KEY_TITLE, tvShow.title)
+        findNavController().navigate(R.id.movie_details_fragment, bundle, options)
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TvShowsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        const val KEY_TITLE = "title"
     }
 }
