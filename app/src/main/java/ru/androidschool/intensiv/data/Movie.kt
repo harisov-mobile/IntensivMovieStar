234d7ae2a36f71bd72fb17e10340a9483bfb5730
harisov-mobile <v8spec@mail.ru>
2021-10-23 17:13:12 +0300

    Добавил MovieApiInterface, MovieApiClient.

--- app/src/main/java/ru/androidschool/intensiv/data/Movie.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/Movie.kt
@@ -1,19 +1,68 @@
 package ru.androidschool.intensiv.data
 
-import java.util.*
+import com.google.gson.annotations.SerializedName
 
-class Movie(
-    var title: String? = "",
-    var voteAverage: Double = 0.0,
-    // добавлены поля
-    var overview: String = "",
-    var studio: String = "",
-    var releaseDate: Date? = null,
-    var previewUrl: String? = "",
-    var genreList: List<Genre> = emptyList(), // список жанров
-    var actorList: List<Actor> = emptyList() // список актеров-исполнителей (актерский состав)
+data class Movie (
 
+    @SerializedName("adult")
+    val isAdult : Boolean,
+
+    @SerializedName("backdrop_path")
+    val backdropPath : String?,  // согласно документации string or null
+
+    @SerializedName("genre_ids")
+    val genreIds : List<Int>,
+
+    @SerializedName("id")
+    val id : Int,
+
+    @SerializedName("original_language")
+    val originalLanguage : String,
+
+    @SerializedName("original_title")
+    val originalTitle : String,
+
+    @SerializedName("overview")
+    val overview : String,
+
+    @SerializedName("popularity")
+    val popularity : Double,
+
+    @SerializedName("poster_path")
+    val posterPath : String?,  // согласно документации string or null
+
+    @SerializedName("release_date")
+    val releaseDate : String,
+
+    @SerializedName("title")
+    val title : String,
+
+    @SerializedName("video")
+    val video : Boolean,
+
+    @SerializedName("vote_average")
+    val voteAverage : Double,
+
+    @SerializedName("vote_count")
+    val voteCount : Int
 ) {
     val rating: Float
         get() = voteAverage.div(2).toFloat()
 }
+
+//data class Movie(
+//
+////    var title: String? = "",
+////    var voteAverage: Double = 0.0,
+////    // добавлены поля
+////    var overview: String = "",
+////    var studio: String = "",
+////    var releaseDate: Date? = null,
+////    var previewUrl: String? = "",
+////    var genreList: List<Genre> = emptyList(), // список жанров
+////    var actorList: List<Actor> = emptyList() // список актеров-исполнителей (актерский состав)
+//
+//) {
+//    val rating: Float
+//        get() = voteAverage.div(2).toFloat()
+//}
--- app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
+++ app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
@@ -1,3 +1,21 @@
 package ru.androidschool.intensiv.network
 
-interface MovieApiInterface
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.androidschool.intensiv.data.MovieResponse
+
+interface MovieApiInterface {
+
+    @GET("movie/now_playing")
+    fun getNowPlayingMovies(@Query("api_key") apiKey: String, @Query("language") language: String
+    ): Call<MovieResponse>
+
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(@Query("api_key") apiKey: String, @Query("language") language: String
+    ): Call<MovieResponse>
+
+    @GET("movie/popular")
+    fun getPopularMovies(@Query("api_key") apiKey: String, @Query("language") language: String
+    ): Call<MovieResponse>
+}
