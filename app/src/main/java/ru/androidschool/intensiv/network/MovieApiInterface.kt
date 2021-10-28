ed3978682e26af2ca301a1f4f9dbf698837b6258
harisov-mobile <v8spec@mail.ru>
2021-10-28 13:26:15 +0300

    В интерфейсе поменял тип вместо Call на Single.

--- app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
+++ app/src/main/java/ru/androidschool/intensiv/network/MovieApiInterface.kt
@@ -1,6 +1,6 @@
 package ru.androidschool.intensiv.network
 
-import retrofit2.Call
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,51 +13,51 @@ interface MovieApiInterface {
     fun getNowPlayingMovies(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieResponse>
+    ): Single<MovieResponse>
 
     @GET("movie/upcoming")
     fun getUpcomingMovies(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieResponse>
+    ): Single<MovieResponse>
 
     @GET("movie/popular")
     fun getPopularMovies(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieResponse>
+    ): Single<MovieResponse>
 
     @GET("tv/popular")
     fun getPopularTvShows(
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<TvShowResponse>
+    ): Single<TvShowResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(
         @Path("movie_id") movieId: Int,
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieDetails>
+    ): Single<MovieDetails>
 
     @GET("movie/{movie_id}/credits")
     fun getMovieCredits(
         @Path("movie_id") movieId: Int,
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieCreditsResponse>
+    ): Single<MovieCreditsResponse>
 
     @GET("tv/{tv_id}")
     fun getTvShowDetails(
         @Path("tv_id") tvShowId: Int,
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<TvShowDetails>
+    ): Single<TvShowDetails>
 
     @GET("tv/{tv_id}/credits")
     fun getTvShowCredits(
         @Path("tv_id") tvShowId: Int,
         @Query("api_key") apiKey: String = BuildConfig.THE_MOVIE_DATABASE_API,
         @Query("language") language: String = "ru"
-    ): Call<MovieCreditsResponse>
+    ): Single<MovieCreditsResponse>
 }
