ff94a290012f1caaacfcbaa6419b909a6f61146a
harisov-mobile <v8spec@mail.ru>
2021-11-10 08:36:26 +0300

    Класс MovieItem теперь работает с классом MovieVO.

--- app/src/main/java/ru/androidschool/intensiv/ui/feed/MovieItem.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/feed/MovieItem.kt
@@ -4,12 +4,12 @@ import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.item_with_text.*
 import ru.androidschool.intensiv.R
-import ru.androidschool.intensiv.data.dto.Movie
+import ru.androidschool.intensiv.data.vo.MovieVO
 import ru.androidschool.intensiv.ui.loadImage
 
 class MovieItem(
-    private val content: Movie,
-    private val onClick: (movie: Movie) -> Unit
+    private val content: MovieVO,
+    private val onClick: (movie: MovieVO) -> Unit
 ) : Item() {
 
     override fun getLayout() = R.layout.item_with_text
