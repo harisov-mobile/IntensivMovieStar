dbf8abb8738ce183ab9569011dc37b87b98cfe88
harisov-mobile <v8spec@mail.ru>
2021-11-19 09:32:52 +0300

    Добавил методы в интерфейс MovieRepositoryDB.

--- app/src/main/java/ru/androidschool/intensiv/domain/repository/MovieRepositoryDB.kt
+++ app/src/main/java/ru/androidschool/intensiv/domain/repository/MovieRepositoryDB.kt
@@ -1,9 +1,26 @@
 package ru.androidschool.intensiv.domain.repository
 
+import io.reactivex.Completable
 import io.reactivex.Single
-import ru.androidschool.intensiv.data.dbo.MovieAndGenreAndActorAndProductionCompany
+import ru.androidschool.intensiv.data.dbo.*
 import ru.androidschool.intensiv.utils.ViewFeature
 
 interface MovieRepositoryDB {
     fun getMovies(viewFeature: ViewFeature): Single<List<MovieAndGenreAndActorAndProductionCompany>>
+
+    fun delete(movieDBO: MovieDBO): Completable
+
+    fun insert(movie: MovieDBO): Completable
+
+    fun insertGenres(genres: List<GenreDBO>): Completable
+
+    fun insertActors(actors: List<ActorDBO>): Completable
+
+    fun insertProductionCompanies(productionCompanies: List<ProductionCompanyDBO>): Completable
+
+    fun insertGenreJoins(joins: List<MovieAndGenreCrossRef>): Completable
+
+    fun insertActorJoins(joins: List<MovieAndActorCrossRef>): Completable
+
+    fun insertProductionCompanyJoins(joins: List<MovieAndProductionCompanyCrossRef>): Completable
 }
